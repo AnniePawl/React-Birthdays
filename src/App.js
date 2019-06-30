ceb5514c76efa5af285c15001825c7fde139ce81
Anna Pawl <annampawl@gmail.com>
2019-06-30 16:53:10 -0700

    bithday component in progress

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import Title from './Title'
+import Birthdays from './Birthdays'
 import './App.css';
 
 function App() {
   return (
     <div>
       < Title title="Birthdays" />
+
+      < Birthdays name="Kathryn Pawl" birthMonth="June" birthDay="12" birthYear="1995" age="21" />
     </div>
   );
 }
---
+++ src/Birthdays.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react'
+
+class Birthdays extends Component {
+  render() {
+    const { name, birthMonth, birthYear, birthDay, age } = this.props;
+
+    return (
+      <div>
+        <div className="name">
+          Name:{name}
+        </div>
+
+        <div className="birthdays birthMonth">
+          {birthMonth}
+        </div>
+
+        <div className="birthdays birthDay">
+          {birthDay}
+        </div>
+
+        <div className="birthdays birthYear">
+          {birthYear}
+        </div>
+
+        <div className="birthdays age">
+          Age: {age}
+        </div>
+      </div>
+    )
+  }
+}
+
+
+export default Birthdays
